0155d3064ca72382ed65169443052fe573905b4a
Edon Ibrahimaj <ei50555@ubt-uni.net>
2022-05-01 22:59:44 +0200

    auth request and user creation

---
+++ server/src/routes/auth.ts
@@ -0,0 +1,64 @@
+import express from "express";
+import { body, validationResult } from "express-validator";
+import User from "../models/User";
+import JWT from "jsonwebtoken";
+
+const router = express.Router();
+
+router.post(
+  "/signup",
+  body("email").isEmail().withMessage("The email is invalid"),
+  body("password")
+    .isLength({ min: 5 })
+    .withMessage("The password is too short"),
+  async (req, res) => {
+    const validationErrors = validationResult(req);
+
+    if (!validationErrors.isEmpty()) {
+      const errors = validationErrors.array().map((error) => {
+        return {
+          msg: error.msg,
+        };
+      });
+
+      return res.json({ errors, data: null });
+    }
+
+    const { email, password } = req.body;
+
+    await User.create({
+      email,
+      password,
+    });
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return res.json({
+        errors: [
+          {
+            msg: "Email already in use",
+          },
+        ],
+        data: null,
+      });
+    }
+    
+
+    const newUser = await User.create({
+      email,
+      password,
+    });
+
+    const token = await JWT.sign(
+      { email: newUser.email },
+      process.env.JWT_SECRET as string,
+      {
+        expiresIn: 360000,
+      }
+    );
+
+    res.json(user);
+  }
+);
+
+export default router;
