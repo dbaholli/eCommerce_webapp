70e8c1b9ade6d3b6681d5803e4df6686d4e5aac9
Altina Ibrahimi <altinaibrahimi123@gmail.com>
2022-06-02 09:33:08 +0200

    login with email password and validate

--- client/src/components/Forms/LoginForm.jsx
+++ client/src/components/Forms/LoginForm.jsx
@@ -1,15 +1,47 @@
 import React, { useState } from "react";
+import { auth, googleAuthProvider } from "../../firebase";
 import { AiOutlineMail } from "react-icons/ai";
 import { BiLock } from "react-icons/bi";
-import { Link } from "react-router-dom";
-// import { ReactComponent as Logo } from "../../assets/Logo.svg";
+import { FcGoogle } from "react-icons/fc";
+import { Link, useHistory } from "react-router-dom";
 import "./LoginForm.scss";
-
-
+import { useDispatch } from "react-redux";
+import { toast } from "react-toastify";
 
 const LoginForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  let dispatch = useDispatch();
+  let history = useHistory();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const result = await auth.signInWithEmailAndPassword(email, password);
+
+      const { user } = result;
+      const idTokenResult = await user.getIdTokenResult();
+
+      dispatch({
+        type: "LOGGED_IN_USER",
+        payload: {
+          // name: user.displayName,
+          email: user.email,
+          token: idTokenResult.token,
+        },
+      });
+      history.push("/");
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+      setLoading(false);
+    }
+  };
+
+
   return (
     <div className="log-in-form-container">
       {/* <h1>
@@ -23,7 +55,7 @@ const LoginForm = () => {
             Enter your details below to log in back to your account
           </p>
         </div>
-        <form action="" className="log-in-form">
+        <form action="" className="log-in-form" onSubmit={handleSubmit}>
           <div className="login-input-container">
             <label htmlFor="email">
               <p>Enter your email address</p>
@@ -62,7 +94,15 @@ const LoginForm = () => {
             <input type="checkbox" id="remember-checkbox" />
             <label htmlFor="remember-checkbox">Remember Me</label>
           </div>
-          <input type="submit" value="Login" />
+          <input
+            type="submit"
+            value="Login"
+            onClick={handleSubmit}
+            disabled={!email || password.length < 6}
+          />
+          <button type="submit" onClick={googleLogin} className="google-login">
+            <FcGoogle /> Login with Google
+          </button>
         </form>
         <p className="dont-have-acc">
           Don't have an account ? <Link to="/register">Register</Link>
