5f3a62fa4ec05f21928a9d40e30e9e07f04b975a
diellbaholli <diell.w40@gmail.com>
2022-07-07 12:18:23 +0200

    new arrivals with pagination

--- client/src/components/Home/NewArrivals.jsx
+++ client/src/components/Home/NewArrivals.jsx
@@ -1,23 +1,31 @@
 import React, { useState, useEffect } from "react";
-import { getProducts } from "../../functions/product";
+import { getProducts, getProductsCount } from "../../functions/product";
 import LoadingCard from "../cards/LoadingCard";
 import ProductCard from "../cards/ProductCard";
 import "./styles/Products.scss";
+import { Pagination } from "antd";
 
 const NewArrivals = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [productsCount, setProductsCount] = useState(0);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     loadAllProducts();
+  }, [page]);
+
+  useEffect(() => {
+    getProductsCount().then((res) => setProductsCount(res.data));
   }, []);
 
   const loadAllProducts = () => {
     setLoading(true);
     // sort, order, limit
-    getProducts("createdAt", "desc", 3).then((res) => {
+    getProducts("createdAt", "desc", page).then((res) => {
       setProducts(res.data);
       setLoading(false);
+      console.log(products);
     });
   };
 
@@ -25,6 +33,7 @@ const NewArrivals = () => {
     <div className="products-section">
       <h1>New Arrivals </h1>
       <div className="products-container">
+        {productsCount}
         {loading ? (
           <LoadingCard count={3} />
         ) : (
@@ -37,6 +46,11 @@ const NewArrivals = () => {
           </>
         )}
       </div>
+      <Pagination
+        current={page}
+        total={(productsCount / 3) * 10}
+        onChange={(value) => setPage(value)}
+      />
     </div>
   );
 };
